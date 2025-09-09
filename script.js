39eec9192f214c927f283e975d26241eb339fb02
Gingersmurlf <theodorenghoff@gmail.com>
2025-09-09 09:12:47 +0200

    nu har jeg tilføjet en del mere ift fetch

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const grid = document.getElementById("grid")
 
 let next = 1
 
-fetch(`https://pokeapi.co/api/v2/pokemon`)
+fetch(`https://pokeapi.co/api/v2/pokemon?limit=1400`)
     .then((svar) => svar.json())
     .then(data => handleData(data))
 
-function getPokemon(url) {
+async function getPokemon(url) {
     return fetch(url)
         .then((svar) => svar.json())
 }
@@ -23,10 +23,11 @@ function handleData(data) {
         allData.forEach(data => {
             html += /*html*/ `
             <div class="container">
-                <img src="${data.sprites.other.dream_world.front_default}">
+                <img src="${data.sprites.other["official-artwork"].front_default ?? "/download.jfif"}">
                 <h2>${data.name}</h2>
+                <h3>${data.types.map(den => den.type.name).join(", ")}</h3>
+                <h4>ID: ${data.id}</h4>
             </div>
-
             `;
         });
         grid.insertAdjacentHTML("beforeend", html);
